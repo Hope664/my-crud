1c39ce7628cda570fb717f0856ee769ef38ca77d
hope664 <umuhozahope5@gmail.com>
2026-05-17 11:22:58 +0200

    working on api

--- api.php
+++ api.php
@@ -1,3 +1,21 @@
 <?php
+header("Content-Type:application/json");
+header("Access-Control-Allow-Origin: *");
 
+$host = "localhost";
+$db = "purovue";
+$user = "root";
+$pass = "uMuHoZa@123hope";
+
+$conn = new mysqli($host,$user,$pass,$db);
+
+if($conn->connect_error){
+exit('connection failed: '. $conn->connect_error);
+}
+echo('connected successfully');
+
+$method = $_SERVER['REQEST_METHOD'];
+if($method=='POST'){
+
+}
 ?>
